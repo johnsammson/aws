3d3b7653057686b8f63092c9283786efd9290b0e
johnsammson <mahesh77361@gmail.com>
2024-08-16 22:47:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,18 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log('MongoDB connection error:', err));
 
 // Routes
 app.get('/', (req, res) => {
   res.send('Code Execution Website API');
 });
 
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/problems', require('./routes/problems'));
 app.use('/api/submissions', require('./routes/submissions'));
 
 // Start the server
